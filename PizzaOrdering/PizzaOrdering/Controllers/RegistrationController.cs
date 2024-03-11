c7cc35e62010ed5669300061a3f4f9204df73928
VladRoboCop <113367165+VladRoboCop@users.noreply.github.com>
2024-03-11 21:18:31 +0200

    Update RegistrationController.cs

--- PizzaOrdering/PizzaOrdering/Controllers/RegistrationController.cs
+++ PizzaOrdering/PizzaOrdering/Controllers/RegistrationController.cs
@@ -12,7 +12,6 @@ public class RegistrationController : Controller
     {
         if (ModelState.IsValid)
         {
-            // Логика обработки регистрации пользователя
             return RedirectToAction("Order", "Product");
         }
         return View(user);
